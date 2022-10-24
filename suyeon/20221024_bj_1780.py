c1836d3790d64be7be756ff554fb70f8bbaf1d5c
suyeon woo <woosean999@gmail.com>
2022-10-24 23:42:13 +0900

    [수연]1780 코드 개선

--- suyeon/20221024_bj_1780.py
+++ suyeon/20221024_bj_1780.py
@@ -1,58 +1,41 @@
 #started at 10:32
-
 n = int(input())
 board = []
 for i in range(n):
     row = list(map(int, input().split()))
     board.append(row)
 
-minus_one_cnt = 0
-zero_cnt = 0
-one_cnt = 0
+answer = {-1:0, 0:0, 1:0}
 
 def recur(start_i, start_j, len):
-    global minus_one_cnt, zero_cnt, one_cnt 
     type = board[start_i][start_j]
     if len == 1:
-        if type == -1:
-            minus_one_cnt += 1 
-        elif type == 0:
-            zero_cnt += 1
-        elif type == 1:
-            one_cnt += 1  
+        answer[type] += 1 
         return
     cnt = 0
+    isPure = True
     for i in range(start_i, start_i + len):
         for j in range(start_j, start_j + len):
             if board[i][j] == type:
                 cnt += 1
             else:
+                isPure = False
                 break
+        if isPure == False:
+            break
+        # For문을 하나만 빠져나오고, 외부 for문은 끝까지 도는 것 주의하기    
 
     if cnt == len ** 2:
-        if type == -1:
-            minus_one_cnt += 1 
-        elif type == 0:
-            zero_cnt += 1
-        elif type == 1:
-            one_cnt += 1  
+        answer[type] += 1 
     else:
         new_len = len // 3
-        recur(start_i, start_j, new_len)
-        recur(start_i, start_j + new_len, new_len)
-        recur(start_i, start_j + 2 * new_len, new_len)
-        
-        recur(start_i + new_len, start_j, new_len)
-        recur(start_i + new_len, start_j + new_len, new_len)
-        recur(start_i + new_len, start_j + 2 * new_len, new_len)
-
-        recur(start_i + 2 * new_len, start_j, new_len)
-        recur(start_i + 2 * new_len, start_j + new_len, new_len)
-        recur(start_i + 2 * new_len, start_j + 2 * new_len, new_len)
+        for di in range(3):
+            for dj in range(3):
+                recur(start_i + di * new_len, start_j + dj * new_len, new_len)
 
 recur(0,0,n)
 
-print(minus_one_cnt)
-print(zero_cnt)
-print(one_cnt)
+for entry in enumerate(answer):
+    print(answer[entry[1]])
 #finished at 10:56
+#코드 개선 결과 코드라인은 짧아진 대신 시간이 500ms정도 증가
