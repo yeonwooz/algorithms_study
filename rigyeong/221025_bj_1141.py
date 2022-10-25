d5d2ad1e99239bc8ea1c8ab0e502c088fdf4d587
rigyeonghong <zxcvbee@naver.com>
2022-10-25 11:41:47 +0900

    풀이

---
+++ rigyeong/221025_bj_1141.py
@@ -0,0 +1,14 @@
+#맞았습니다!
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+words = [(sys.stdin.readline().strip()) for _ in range(n)]
+words.sort()
+q = deque()
+for word in words:
+    if q:
+        len_q_word = len(q[-1])
+        if q[-1] in word[:len_q_word]:
+            q.pop()
+    q.append(word)
+print(len(q))
