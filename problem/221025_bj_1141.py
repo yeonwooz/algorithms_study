b0cad9ef992de9ef840d3789a37bbe96e9f70da5
suyeon woo <woosean999@gmail.com>
2022-10-25 11:51:16 +0900

    problem/1141

---
+++ problem/221025_bj_1141.py
@@ -0,0 +1,20 @@
+#1141 접두사 예린
+# https://www.acmicpc.net/problem/1141
+
+import sys
+input = sys.stdin.readline
+N = int(input())
+str_lst = [ input().strip() for _ in range (N) ]
+
+result_set = set()
+
+for s1 in str_lst:
+    Flag = 0
+    for s2 in str_lst:
+        if s1 == s2: continue
+        elif s2.startswith(s1):
+            Flag = 1
+            break
+    if Flag == 0: result_set.add(s1)
+
+print(len(result_set))
