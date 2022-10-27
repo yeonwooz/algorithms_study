790df86075f3e8f1b791d12b9e2a7c37b0dc4942
suyeon woo <woosean999@gmail.com>
2022-10-28 01:08:31 +0900

    [수연] 코드 개선

--- suyeon/20221028_bj_11497.py
+++ suyeon/20221028_bj_11497.py
@@ -1,30 +1,18 @@
 #11497 통나무 건너뛰기 우수연
 # started at 12:02
 
-def get_max_diff(arr):
-    l = len(arr)
-    diff = abs(arr[l-1] - arr[0])
-    for i in range(l-1):
-        diff = max(diff, abs(arr[i]-arr[i+1]))
-    return diff
-
-def solve(trees):
-    answer = []
-    i = 0
-    while trees: 
-        popped = trees.pop()
-
-        if i % 2 > 0:
-            answer = [popped] + answer
-        elif i % 2 == 0:
-            answer.append(popped)
-        i += 1
-    df = get_max_diff(answer)
-    print(df)
+def solve(trees, N):
+    max_diff = 0
+    for i in range(N - 2):
+        diff = trees[i+2] - trees[i]
+        # 2 차이 일 때 가장 평탄하다
+        if diff > max_diff:
+            max_diff = diff
+    print(max_diff)
 
 T = int(input())
 for _ in range(T):
     N = int(input())
     trees = list(map(int, input().split()))
     trees.sort()
-    solve(trees) 
+    solve(trees, N) 
