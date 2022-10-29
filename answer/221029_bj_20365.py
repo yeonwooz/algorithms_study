c53bc3b83ed4c329c5741989898a6bb798799b70
rigyeonghong <zxcvbee@naver.com>
2022-10-29 11:42:23 +0900

    풀이

--- answer/221029_bj_20365.py
+++ answer/221029_bj_20365.py
@@ -1,2 +1,17 @@
 #블로그2 홍리경
 #https://www.acmicpc.net/problem/20365
+
+import sys
+readl = sys.stdin.readline
+putnum = int(readl())
+putstr = readl().strip()
+prev = putstr[0]
+cnt= 1
+for char in putstr:
+    if prev != char:
+        if char != putstr[0]:
+            cnt += 1
+            prev = char
+        else:
+            prev = char
+print(cnt)
