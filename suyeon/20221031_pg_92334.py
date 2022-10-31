0400042398fb6b1582b4d5cd852dd25de8dbf57c
suyeon woo <woosean999@gmail.com>
2022-10-31 11:18:43 +0900

    [수연]프로그래머스 92334

---
+++ suyeon/20221031_pg_92334.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+def solution(id_list, report,k):
+    answer = []
+    report = list(set(report))  # 중복 신고 제거
+    reporters = defaultdict(set) # 이메일 보낼 대상들 (초기값 => [])
+    reported_cnts = defaultdict(int) # 신고당한 횟수 (초기값  => 0)
+	
+    for r in report:
+        cur_user, reported = r.split()
+        # 신고자가 신고한 id 추가
+        reporters[cur_user].add(reported)
+        # 신고당한 id의 신고 횟수 추가
+        reported_cnts[reported] += 1
+    
+    for i in id_list:
+        reporting_targets = reporters[i]  # i번째 유저가 신고한 대상들
+        cnt = 0   # 신고한 대상들 중 몇명이 정지될까?
+        for target in reporting_targets:  
+            if reported_cnts[target]>=k: # 이번 대상이 정지되었다
+                cnt += 1     
+        answer.append(cnt)
+    return answer
