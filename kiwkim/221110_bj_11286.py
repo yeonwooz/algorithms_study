5e058e3265361af9375d381594bdec7c2988a79d
kiwkim <atthenoon@gmail.com>
2022-11-10 23:36:07 +0900

    add. 주석 추가

--- kiwkim/221110_bj_11286.py
+++ kiwkim/221110_bj_11286.py
@@ -3,23 +3,23 @@
 import sys
 import heapq
 readl = sys.stdin.readline
-
+## 음수 힙과 양수 힙을 따로 만들고 출력 연산시. 두 힙의 루트를 비교해서 더 절댓값이 작은 놈을 출력.
 n = int(readl())
-heap_minus = []
-heap_plus = []
+heap_minus = [] ## 음수
+heap_plus = [] ## 양수
 cnt = 0
 for i in range(n):
     put = int(readl())
     if put > 0:
-        heapq.heappush(heap_plus, put);
+        heapq.heappush(heap_plus, put); 
     elif put < 0:
-        heapq.heappush(heap_minus, -put);
+        heapq.heappush(heap_minus, -put); ## 음수는 음수 곱해서 넣음.
     elif put == 0:
         if heap_minus and heap_plus:
             if heap_minus[0] > heap_plus[0]:
-                print(heapq.heappop(heap_plus))
+                print(heapq.heappop(heap_plus)) 
             else:
-                print(-heapq.heappop(heap_minus))
+                print(-heapq.heappop(heap_minus)) ## 음수힙팝 음수 곱해서 팝함
         elif heap_minus:
             print(-heapq.heappop(heap_minus))
         elif heap_plus:
